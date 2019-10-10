baabdd044896db36c78bd5af89b90c255d7af7ba
Alice R <alice.rouben@gmail.com>
2019-10-10 16:20:12 +0200

    added required fields (html) + placeholders for select/options objects + link to sent page

--- index.php
+++ index.php
@@ -20,19 +20,20 @@
 
             <div class="row justify-content-center">
 
-                <form action="#" method="post" class="col-xs-12 col-lg-10" id="form">
+                <form action="result.php" method="post" class="col-xs-12 col-lg-10" id="form">
                     
                     <div class="row justify-content-center" id="content">
-                        <input type="text" name="fname" placeholder="first name" class="col-10">
-                        <input type="text" name="lname" placeholder="last name" class="col-10">
+                        <input type="text" name="fname" placeholder="first name" class="col-10" required>
+                        <input type="text" name="lname" placeholder="last name" class="col-10" required>
                     </div>
                     
                     <div class="row justify-content-center" id="content">
-                        <input type="email" name="mail" placeholder="email@mailexample.com" class="col-10">
+                        <input type="email" name="mail" placeholder="email@mailexample.com" class="col-10" required>
                     </div>
                     
                     <div class="row justify-content-center" id="content">
-                        <select name="Country" class="col-10">
+                        <select name="Country" class="col-10" required>
+                            <option value="" disabled selected>Select your country</option>
                             <option value="Belgium">Belgium</option>
                             <option value="Luxemburg">Luxemburg</option>
                             <option value="France">France</option>
@@ -49,7 +50,8 @@
                     </div>
 
                     <div class="row justify-content-center" id="content">
-                        <select name="subject" placeholder="problem category" class="col-10">
+                        <select name="subject" placeholder="problem category" class="col-10" required>
+                            <option value="" disabled selected>Select your problem</option>
                             <option value="broken">My package arrived damaged</option>
                             <option value="missing">My package is missing</option>
                             <option value="other">Other</option>
@@ -57,7 +59,7 @@
                     </div>
 
                     <div class="row justify-content-center" id="content">
-                        <textarea name="message"  cols="30" rows="10" placeholder="your message here"></textarea>
+                        <textarea name="message"  cols="30" rows="10" placeholder="your message here" required></textarea>
                     </div>
                     
                     <div class="row justify-content-end" id="content">
@@ -73,6 +75,5 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src=""></script>
     </body>
 </html>
